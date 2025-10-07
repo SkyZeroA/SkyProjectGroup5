f893f516f16026a3dd965d2b171f0d4204cfcc94
Harry Taylor <harrytaylor170503@gmail.com>
2025-10-07 13:35:59 +0100

    EDIT: all functions work in routes now

--- backend/routes.py
+++ backend/routes.py
@@ -3,7 +3,7 @@ from backend import app
 from hashlib import sha256
 
 from Questionnaire import Questionnaire
-from data_access import read_view_table,check_password,read_user_table
+from data_access import *
 
 
 @app.route('/', methods=['GET', 'POST'])
