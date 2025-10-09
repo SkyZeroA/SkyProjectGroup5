afaa0b9753a77b4c23ea087140b1849a5e4bca71
Harry Taylor <harrytaylor170503@gmail.com>
2025-10-09 09:25:55 +0100

    EDIT: Message added to app.py to show how to run the flask server

--- backend/app.py
+++ backend/app.py
@@ -6,3 +6,7 @@ if __name__ == "__main__":
     db.init_db()
     db.init_insert()
     app.run(port=9099, debug=True)
+
+# To run this you need to be in the main project folder "SkyProjectGropu5"
+# Then run the following command:
+# python3 -m backend.app
