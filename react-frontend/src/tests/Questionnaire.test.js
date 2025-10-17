a34699ca1a329d5d74fd4298c4ce9d8c6fc408e9
Salahuddin <adnan.salah84@gmail.com>
2025-10-17 17:14:13 +0100

    test: all questions and submit button

--- react-frontend/src/tests/Questionnaire.test.js
+++ react-frontend/src/tests/Questionnaire.test.js
@@ -14,3 +14,26 @@ jest.mock('react-router-dom', () => ({
 
 
 const Questionnaire = require('../screens/Questionnaire').default;
+
+
+test("renders all questions and submit button", () => {
+    render(<Questionnaire />);
+
+    expect(
+      screen.getByText(/Welcome to ClearSky/i)
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText(/Question 1: How do you usually commute to work\?/i)
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(/Question 2: What type of diet do you typically follow\?/i)
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(/Question 3: How energy-efficient is your home\?/i)
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole("button", { name: /Continue/i })
+    ).toBeInTheDocument();
+  });
