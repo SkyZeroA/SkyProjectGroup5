a4b7f9d54accd612793f76ee78c05bc517e16dce
adnansalah84 <adnan.salah84@gmail.com>
2025-11-01 14:48:23 +0000

    Update: Sign in and Sign up test

--- react-frontend/src/tests/__mocks__/react-router-dom.js
+++ react-frontend/src/tests/__mocks__/react-router-dom.js
@@ -12,4 +12,24 @@ function Route({ element }) {
   return element || null;
 }
 
-module.exports = { BrowserRouter, Routes, Route };
+function MemoryRouter({ children }) {
+  return React.createElement(React.Fragment, null, children);
+}
+
+function useLocation() {
+  return { pathname: '/' };
+}
+
+function Navigate() {
+  return null;
+}
+
+function useNavigate() {
+  return () => {};
+}
+
+function Link({ children, to, className, ...props }) {
+  return React.createElement('a', { href: to || '#', className, ...props }, children);
+}
+
+module.exports = { BrowserRouter, Routes, Route, MemoryRouter, useLocation, Navigate, useNavigate, Link };
